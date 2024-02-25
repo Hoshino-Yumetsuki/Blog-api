51a994a6444b4a3c9a55dc1a603fc0f55ead5b21
Anjiurine <q78kg.1ffrj6@outlook.com>
2024-02-25 20:47:30 +0800

    .

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -6,7 +6,6 @@ export default {
         text: 'API',
         items: [
           { text: 'HOME', link: '/' },
-          { text: 'Github Calendar API', link: '/github-calendar-api.md' },
           { text: 'Web Img Compression API', link: '/web-img-compression-api.md' },
           { text: 'Random Pixiv Pictures', link: '/random-pixiv-pictures.md' },
           { text: 'G2O API', link: '/gemini2openai-api.md' },
